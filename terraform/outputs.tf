f9de4462822b23931971e6b8e5641ede4de5611f
josex2r <jlrepresa@gmail.com>
2022-10-28 10:32:27 +0200

    ci: deploy using actions + terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+# Display the service URL
+output "service_url" {
+  value = google_cloud_run_service.run_service.status[0].url
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "project_id" {}
+
+variable "region" {}
+
+variable "app_name" {}
+
+variable "image" {}

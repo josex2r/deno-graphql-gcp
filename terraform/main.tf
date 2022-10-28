7907bc680c846bfadf50b4d29d27093e76547f31
josex2r <jlrepresa@gmail.com>
2022-10-28 10:32:27 +0200

    ci: deploy using actions + terraform

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    # Cloud Run support was added on 3.3.0
+    google = ">= 3.3"
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
+# Create the Cloud Run service
+resource "google_cloud_run_service" "run_service" {
+  name = var.app_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/google-samples/hello-app:1.0"
+      }
+    }
+  }
+
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+}
+
+# Allow unauthenticated users to invoke the service
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
